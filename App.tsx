fcc7dceeae0a503e997dd6c0049f5b46c79de6c5
JOSHUA-CHUA-12 <jchua378@gmail.com>
2025-01-09 00:16:19 +0800

    build(auth): stack navigation (auth)

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import startscreen from "./auth_screens/startscreen";
+import StartScreen from "./auth_screens/StartScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
+import SignIn from "./auth_screens/signin";
+import SignUp from "./auth_screens/signup";
+import HomeScreen from "./auth_screens/homescreen";
 
 export type RootStackNavigatorParamsList = {
-    startscreen: undefined;
-    signup: undefined;
-    signin: undefined;
-    homescreen: undefined;
+    Startscreen: undefined;
+    SignUp: undefined;
+    SignIn: undefined;
+    HomeScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackNavigatorParamsList>();
@@ -18,25 +21,25 @@ export default function App() {
     return (
         <SafeAreaProvider>
             <NavigationContainer>
-                <Stack.Navigator initialRouteName="startscreen">
+                <Stack.Navigator initialRouteName="Startscreen">
                     <Stack.Screen
-                        name="startscreen"
-                        component={startscreen}
+                        name="Startscreen"
+                        component={StartScreen}
                         options={{ headerShown: false }}
                     />
                     <Stack.Screen
-                        name="signup"
-                        component={startscreen}
+                        name="SignUp"
+                        component={SignUp}
                         options={{ headerShown: false }}
                     />
                     <Stack.Screen
-                        name="signin"
-                        component={startscreen}
+                        name="SignIn"
+                        component={SignIn}
                         options={{ headerShown: false }}
                     />
                     <Stack.Screen
-                        name="homescreen"
-                        component={startscreen}
+                        name="HomeScreen"
+                        component={HomeScreen}
                         options={{ headerShown: false }}
                     />
                 </Stack.Navigator>
