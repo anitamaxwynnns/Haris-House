3b4eaf95a1d184bb74f108d3a49ba56f1a42d605
JOSHUA-CHUA-12 <jchua378@gmail.com>
2025-01-08 23:38:18 +0800

    build: stack navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import startscreen from "./auth_screens/startscreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+
+export type RootStackNavigatorParamsList = {
+    startscreen: undefined;
+    signup: undefined;
+    signin: undefined;
+    homescreen: undefined;
+};
+
+const Stack = createStackNavigator<RootStackNavigatorParamsList>();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="startscreen">
+                    <Stack.Screen
+                        name="startscreen"
+                        component={startscreen}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="signup"
+                        component={startscreen}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="signin"
+                        component={startscreen}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="homescreen"
+                        component={startscreen}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
