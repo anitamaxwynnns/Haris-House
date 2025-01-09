0ef9cd3c8cf695f5fee869b1f69e72b351c63b68
JOSHUA-CHUA-12 <jchua378@gmail.com>
2025-01-09 12:43:20 +0800

    build: rename homescreen file

---
+++ auth_screens/Profile.tsx
@@ -0,0 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function Profile() {
+    return (
+        <SafeAreaProvider>
+            <View style={{flex:1}}>
+
+            </View>
+
+        </SafeAreaProvider>
+    );
+}
+
